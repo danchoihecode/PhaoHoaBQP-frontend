22262afc68db4a1b17ef67713270870fdaf38f84
danchoihecode <hoanghai2003vp@gmail.com>
2025-11-02 17:24:38 +0700

    Update news

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,9 +1,9 @@
 <template>
         <div class="flex flex-col justify-center sm:bg-[#F1F2F5] pb-3">
                 <div class="content pt-10">
-                        <div class="title">Giới thiệu về Bếp Nam Dương</div>
+                        <div class="title">Pháo hoa Z121 Bộ Quốc Phòng</div>
                         <div class="date-and-view">
-                                <div><span>Cập nhật ngày 28/08/2024</span></div>
+                                <div><span>Cập nhật ngày 02/11/2025</span></div>
                                 <div class="flex items-center gap-[3px]"><img
                                                 src="data:image/svg+xml,%3csvg%20width=&#39;24&#39;%20height=&#39;24&#39;%20viewBox=&#39;0%200%2024%2024&#39;%20fill=&#39;none&#39;%20xmlns=&#39;http://www.w3.org/2000/svg&#39;%3e%3cpath%20d=&#39;M15.5799%2011.9999C15.5799%2013.9799%2013.9799%2015.5799%2011.9999%2015.5799C10.0199%2015.5799%208.41992%2013.9799%208.41992%2011.9999C8.41992%2010.0199%2010.0199%208.41992%2011.9999%208.41992C13.9799%208.41992%2015.5799%2010.0199%2015.5799%2011.9999Z&#39;%20stroke=&#39;%23ABADAF&#39;%20stroke-width=&#39;1.5&#39;%20stroke-linecap=&#39;round&#39;%20stroke-linejoin=&#39;round&#39;/%3e%3cpath%20d=&#39;M11.9999%2020.27C15.5299%2020.27%2018.8199%2018.19%2021.1099%2014.59C22.0099%2013.18%2022.0099%2010.81%2021.1099%209.39997C18.8199%205.79997%2015.5299%203.71997%2011.9999%203.71997C8.46984%203.71997%205.17984%205.79997%202.88984%209.39997C1.98984%2010.81%201.98984%2013.18%202.88984%2014.59C5.17984%2018.19%208.46984%2020.27%2011.9999%2020.27Z&#39;%20stroke=&#39;%23ABADAF&#39;%20stroke-width=&#39;1.5&#39;%20stroke-linecap=&#39;round&#39;%20stroke-linejoin=&#39;round&#39;/%3e%3c/svg%3e"
                                                 alt="icon"><span>23 lượt
@@ -11,27 +11,17 @@
                         </div>
                         <div class="summary">
                                 <div class="my-8 newContent">
-                                        <p><img src="/news.png" alt=""></p>
+                                        <p><img src="/phao-hoa-banner.png" alt=""></p>
                                 </div>
                         </div>
                         <div>
                                 <div class="recommend-news">
                                         <div class="text-grey-800 font-semibold"> Xem thêm </div>
                                         <ul class="list-disc list-items">
-                                                <li><router-link :to="`/news`">Khuyến mại</router-link></li>
-                                                <li><router-link :to="`/news`">Kinh nghiệm hay</router-link></li>
-                                                <li><router-link :to="`/news`">Hệ thống Showroom</router-link></li>
-                                                <li><router-link :to="`/news`">Đăng ký gia hạn bảo hành</router-link>
-                                                </li>
-                                                <li><router-link :to="`/news`">Chính sách thanh toán</router-link></li>
-                                                <li><router-link :to="`/news`">Đăng ký khảo sát tư vấn lắp đặt tại
-                                                                nhà</router-link></li>
-                                                <li><router-link :to="`/news`">Giao hàng và lắp đặt</router-link></li>
-                                                <li><router-link :to="`/news`">Quyền lợi sau mua hàng</router-link></li>
-                                                <li><router-link :to="`/news`">Bảo hành và đổi sản phẩm</router-link>
-                                                </li>
-                                                <li><router-link :to="`/news`">Quy định chung</router-link></li>
-
+                                                <li><router-link :to="`/news`">Chính sách bảo mật</router-link></li>                                                                  
+                                                <li><router-link :to="`/news`">Chính sách đổi trả</router-link> </li>
+                                                <li><router-link :to="`/news`">Chính sách vận chuyển</router-link></li>
+                                                <li><router-link :to="`/news`">Hướng dẫn sử dụng</router-link></li>
                                         </ul>
                                 </div>
                         </div>
